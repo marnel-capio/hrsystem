0d5b2a4926b779aec133d9c322026e2144b3abbc
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-02-24 10:12:13 +0800

    Update web.php with local changes

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,7 @@ Route::get('dashboard', function () {
 
     // ACTION
     Route::get('/action', fn () => Inertia::render('action/Action'))->name('action.index');
-    Route::get('/action/applications', fn () => Inertia::render('action/Applications'))->name('action.applications');
-    Route::get('/action/batches', action: [ActionBatchController::class, 'index'])->name('action.list');    
     Route::get('/action/batches/create', [ActionBatchController::class, 'index'])->name('action.create');    
-
     Route::get('/action/batches', [ActionBatchController::class, 'index'])
     ->middleware(['auth'])
     ->name('action.list');
