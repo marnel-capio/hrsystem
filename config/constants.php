f2ee4e232b9403677112f98d98275b7980fd04b0
AWSYS-I\nemo.gajes <nemo.gajes@awsys-i.com>
2026-02-25 08:54:55 +0800

    HRD - 17: Minor Menu revision, error prompt rollback

--- config/constants.php
+++ config/constants.php
@@ -10,16 +10,16 @@ return [
         '/action/applicants' => [1, 2, 3, 5, 6],
         '/action/applications' => [1, 2, 3, 5, 6],
         '/intermediate/projects' => [1, 5],
-        '/intermediate/resource-requisitions' => [1, 5],
+        '/intermediate/resource-requisitions' => [1, 2, 3, 5],
         '/intermediate/applicants' => [1, 2, 3, 5, 6],
         '/intermediate/applications' => [1, 2, 3, 5, 6],
-        '/dashboard' => [1,2,3,5], // only users allowed to see dashboard
+        '/dashboard' => [1,2,3,5], 
         '/walk-in-application' => [1, 7],
         '/account/settings' => [1,2,3,4,5,6],
     ],
     'hiddenLinks' => [
         '/application-tracker' => [4, 7], 
-        '/application-tracker-dashboard' => [4, 6, 7], // hide dashboard for these
+        '/application-tracker-dashboard' => [4, 6, 7], 
         '/action' => [4, 7],
         '/intermediate' => [4, 7],
         '/account/settings' => [7],
