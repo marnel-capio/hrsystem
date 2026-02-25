81bd444c1bc20fcca417d47e9ea4e49fbc1f334d
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-02-25 16:00:01 +0800

    HRD-18: List Action Bathc

--- app/Http/Controllers/ActionBatchController.php
+++ app/Http/Controllers/ActionBatchController.php
@@ -19,7 +19,7 @@ class ActionBatchController extends Controller
  
         $search = $request->input('search');
  
-        $batches = ActionBatchModel::getPaginated($search, 10);
+        $batches = ActionBatchModel::getPaginated($search, 20);
         $batchesTotal = ActionBatchModel::count();
  
         return Inertia::render('action/batches/ActionBatchList', [
