ecb5adfce69e87ebd87c2eb5e6edf1d1f1c00bb3
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-03-02 09:04:04 +0800

    HRD-18: List ACtion Batch

--- resources/js/actions/Laravel/Fortify/Http/Controllers/index.ts
+++ resources/js/actions/Laravel/Fortify/Http/Controllers/index.ts
@@ -1,5 +1,3 @@
-import AuthenticatedSessionController from './AuthenticatedSessionController'
-import PasswordResetLinkController from './PasswordResetLinkController'
 import NewPasswordController from './NewPasswordController'
 import RegisteredUserController from './RegisteredUserController'
 import EmailVerificationPromptController from './EmailVerificationPromptController'
@@ -14,9 +12,7 @@ import TwoFactorQrCodeController from './TwoFactorQrCodeController'
 import TwoFactorSecretKeyController from './TwoFactorSecretKeyController'
 import RecoveryCodeController from './RecoveryCodeController'
 const Controllers = {
-    AuthenticatedSessionController: Object.assign(AuthenticatedSessionController, AuthenticatedSessionController),
-PasswordResetLinkController: Object.assign(PasswordResetLinkController, PasswordResetLinkController),
-NewPasswordController: Object.assign(NewPasswordController, NewPasswordController),
+    NewPasswordController: Object.assign(NewPasswordController, NewPasswordController),
 RegisteredUserController: Object.assign(RegisteredUserController, RegisteredUserController),
 EmailVerificationPromptController: Object.assign(EmailVerificationPromptController, EmailVerificationPromptController),
 VerifyEmailController: Object.assign(VerifyEmailController, VerifyEmailController),
