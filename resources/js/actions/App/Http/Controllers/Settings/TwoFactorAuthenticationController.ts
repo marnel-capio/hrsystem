f7bc200871579a47598406cf6b904e644fd64277
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-03-17 11:36:30 +0800

    EditDetail

--- resources/js/actions/App/Http/Controllers/Settings/TwoFactorAuthenticationController.ts
+++ resources/js/actions/App/Http/Controllers/Settings/TwoFactorAuthenticationController.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\Settings\TwoFactorAuthenticationController::show
  * @see app/Http/Controllers/Settings/TwoFactorAuthenticationController.php:28
