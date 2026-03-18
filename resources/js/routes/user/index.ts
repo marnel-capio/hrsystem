80cf2ff05ac846fb4fb326e9605216d0826b56dc
AWSYS-I\nemo.gajes <nemo.gajes@awsys-i.com>
2026-03-18 08:49:52 +0800

    HRD - 57: conflicts

--- resources/js/routes/user/index.ts
+++ resources/js/routes/user/index.ts
@@ -1,6 +1,91 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../wayfinder'
 /**
- * @see routes/web.php:61
+* @see \App\Http\Controllers\UserController::update
+ * @see app/Http/Controllers/UserController.php:95
+ * @route '/user/{id}/update'
+ */
+export const update = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
+    url: update.url(args, options),
+    method: 'put',
+})
+
+update.definition = {
+    methods: ["put"],
+    url: '/user/{id}/update',
+} satisfies RouteDefinition<["put"]>
+
+/**
+* @see \App\Http\Controllers\UserController::update
+ * @see app/Http/Controllers/UserController.php:95
+ * @route '/user/{id}/update'
+ */
+update.url = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions) => {
+    if (typeof args === 'string' || typeof args === 'number') {
+        args = { id: args }
+    }
+
+    
+    if (Array.isArray(args)) {
+        args = {
+                    id: args[0],
+                }
+    }
+
+    args = applyUrlDefaults(args)
+
+    const parsedArgs = {
+                        id: args.id,
+                }
+
+    return update.definition.url
+            .replace('{id}', parsedArgs.id.toString())
+            .replace(/\/+$/, '') + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\UserController::update
+ * @see app/Http/Controllers/UserController.php:95
+ * @route '/user/{id}/update'
+ */
+update.put = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteDefinition<'put'> => ({
+    url: update.url(args, options),
+    method: 'put',
+})
+
+    /**
+* @see \App\Http\Controllers\UserController::update
+ * @see app/Http/Controllers/UserController.php:95
+ * @route '/user/{id}/update'
+ */
+    const updateForm = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+        action: update.url(args, {
+                    [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                        _method: 'PUT',
+                        ...(options?.query ?? options?.mergeQuery ?? {}),
+                    }
+                }),
+        method: 'post',
+    })
+
+            /**
+* @see \App\Http\Controllers\UserController::update
+ * @see app/Http/Controllers/UserController.php:95
+ * @route '/user/{id}/update'
+ */
+        updateForm.put = (args: { id: string | number } | [id: string | number ] | string | number, options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
+            action: update.url(args, {
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'PUT',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'post',
+        })
+    
+    update.form = updateForm
+/**
+* @see \App\Http\Controllers\UserController::index
+ * @see app/Http/Controllers/UserController.php:23
  * @route '/user'
  */
 export const index = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
@@ -14,7 +99,8 @@ index.definition = {
 } satisfies RouteDefinition<["get","head"]>
 
 /**
- * @see routes/web.php:61
+* @see \App\Http\Controllers\UserController::index
+ * @see app/Http/Controllers/UserController.php:23
  * @route '/user'
  */
 index.url = (options?: RouteQueryOptions) => {
@@ -22,7 +108,8 @@ index.url = (options?: RouteQueryOptions) => {
 }
 
 /**
- * @see routes/web.php:61
+* @see \App\Http\Controllers\UserController::index
+ * @see app/Http/Controllers/UserController.php:23
  * @route '/user'
  */
 index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
@@ -30,7 +117,8 @@ index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
     method: 'get',
 })
 /**
- * @see routes/web.php:61
+* @see \App\Http\Controllers\UserController::index
+ * @see app/Http/Controllers/UserController.php:23
  * @route '/user'
  */
 index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
@@ -39,7 +127,8 @@ index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
 })
 
     /**
- * @see routes/web.php:61
+* @see \App\Http\Controllers\UserController::index
+ * @see app/Http/Controllers/UserController.php:23
  * @route '/user'
  */
     const indexForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
@@ -48,7 +137,8 @@ index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     })
 
             /**
- * @see routes/web.php:61
+* @see \App\Http\Controllers\UserController::index
+ * @see app/Http/Controllers/UserController.php:23
  * @route '/user'
  */
         indexForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
@@ -56,7 +146,8 @@ index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
             method: 'get',
         })
             /**
- * @see routes/web.php:61
+* @see \App\Http\Controllers\UserController::index
+ * @see app/Http/Controllers/UserController.php:23
  * @route '/user'
  */
         indexForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
