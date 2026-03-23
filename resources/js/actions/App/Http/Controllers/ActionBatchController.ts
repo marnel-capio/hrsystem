8442d03d5a9e6af8c85abce79e219650bae38acd
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-03-23 09:18:26 +0800

    HRD-20: Edit page Action Batch name field

--- resources/js/actions/App/Http/Controllers/ActionBatchController.ts
+++ resources/js/actions/App/Http/Controllers/ActionBatchController.ts
@@ -1,4 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\ActionBatchController::index
  * @see app/Http/Controllers/ActionBatchController.php:22
