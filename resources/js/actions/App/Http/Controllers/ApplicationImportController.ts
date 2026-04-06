c9f6f67d4b55d1436a85e96ae857c0986e224c43
AWSYS-I\abijah.galwan <abijah.galwan@awsys-i.com>
2026-04-06 17:33:25 +0800

    hrd-120

--- resources/js/actions/App/Http/Controllers/ApplicationImportController.ts
+++ resources/js/actions/App/Http/Controllers/ApplicationImportController.ts
@@ -1,7 +1,7 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\ApplicationImportController::importMethod
- * @see app/Http/Controllers/ApplicationImportController.php:46
+ * @see app/Http/Controllers/ApplicationImportController.php:47
  * @route '/action/applications/import'
  */
 export const importMethod = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
@@ -16,7 +16,7 @@ importMethod.definition = {
 
 /**
 * @see \App\Http\Controllers\ApplicationImportController::importMethod
- * @see app/Http/Controllers/ApplicationImportController.php:46
+ * @see app/Http/Controllers/ApplicationImportController.php:47
  * @route '/action/applications/import'
  */
 importMethod.url = (options?: RouteQueryOptions) => {
@@ -25,7 +25,7 @@ importMethod.url = (options?: RouteQueryOptions) => {
 
 /**
 * @see \App\Http\Controllers\ApplicationImportController::importMethod
- * @see app/Http/Controllers/ApplicationImportController.php:46
+ * @see app/Http/Controllers/ApplicationImportController.php:47
  * @route '/action/applications/import'
  */
 importMethod.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
@@ -35,7 +35,7 @@ importMethod.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
 
     /**
 * @see \App\Http\Controllers\ApplicationImportController::importMethod
- * @see app/Http/Controllers/ApplicationImportController.php:46
+ * @see app/Http/Controllers/ApplicationImportController.php:47
  * @route '/action/applications/import'
  */
     const importMethodForm = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
@@ -45,7 +45,7 @@ importMethod.post = (options?: RouteQueryOptions): RouteDefinition<'post'> => ({
 
             /**
 * @see \App\Http\Controllers\ApplicationImportController::importMethod
- * @see app/Http/Controllers/ApplicationImportController.php:46
+ * @see app/Http/Controllers/ApplicationImportController.php:47
  * @route '/action/applications/import'
  */
         importMethodForm.post = (options?: RouteQueryOptions): RouteFormDefinition<'post'> => ({
