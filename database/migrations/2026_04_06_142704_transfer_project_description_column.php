82e93c59a2108238d0257c3b76478909147fdca5
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-04-08 11:24:23 +0800

    List RRF

--- database/migrations/2026_04_06_142704_transfer_project_description_column.php
+++ database/migrations/2026_04_06_142704_transfer_project_description_column.php
@@ -1,13 +1,3 @@
-php artisan make:migration transfer_project_description_column
-```
-
-This will create a migration file in the `database/migrations` folder.
-
-#### Step 2: Define the Migration
-
-In the newly created migration file (e.g., `2023_04_06_123456_transfer_project_description_column.php`), you'll define the steps to transfer the column.
-
-```php
 <?php
 
 use Illuminate\Database\Migrations\Migration;
