aebe9a7a2b847af3732e9d4c25e61f3fe0746062
AWSYS-I\abijah.galwan <abijah.galwan@awsys-i.com>
2026-04-13 16:51:19 +0800

    4-13 demo resource schedule codefixes

--- resources/js/pages/action/schedules/ResourceScheduleEdit.vue
+++ resources/js/pages/action/schedules/ResourceScheduleEdit.vue
@@ -84,6 +84,25 @@ function formatActivityName(key: string) {
   return names[key] || key;
 }
 
+function previousActivityEnd(activity: string) {
+  const index = ganttActivities.indexOf(activity);
+  if (index <= 0) return "";
+  const prev = ganttActivities[index - 1];
+  return ganttForm.value[prev].end || "";
+}
+
+function endMinWeek(activity: string) {
+  if (activity === 'training') return "";
+
+  const ownStart = ganttForm.value[activity].start || "";
+  const prevEnd = previousActivityEnd(activity);
+
+  if (ownStart && prevEnd) {
+    return ownStart > prevEnd ? ownStart : prevEnd;
+  }
+
+  return ownStart || prevEnd || deploymentMinWeek.value;
+}
 const selectedBatchName = computed(() => {
   return props.currentBatch?.action_batch || 'Batch';
 });
@@ -346,6 +365,27 @@ onMounted(() => {
   }
 });
 
+watch(
+  ganttForm,
+  (newVal) => {
+    ganttActivities.forEach((act) => {
+      if (act === 'training') return;
+
+      const row = newVal[act];
+
+      if (row.end && endMinWeek(act) && row.end < endMinWeek(act)) {
+        row.end = "";
+      }
+
+      if (row.start && startMinWeek(act) && row.start < startMinWeek(act)) {
+        row.start = "";
+        row.end = "";
+      }
+    });
+  },
+  { deep: true }
+);
+
 watch(successMessage, (newVal) => {
   if (newVal) {
     showSuccess.value = true;
@@ -523,7 +563,7 @@ watch(
                       v-model="ganttForm[act].end"
                       class="w-full bg-zinc-50 border rounded-lg p-2"
                       :disabled="!canEditEnd(act)"
-                      :min="ganttForm[act].start || deploymentMinWeek"
+                      :min="endMinWeek(act)"
                       :max="deploymentMaxWeek"
                     />
                   </div>
