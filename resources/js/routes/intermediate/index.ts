cfc248cd99b2f4555de774ece44e0ee042c2137d
AWSYS-I\nemo.gajes <nemo.gajes@awsys-i.com>
2026-04-13 14:22:52 +0800

    ts

---
+++ resources/js/routes/intermediate/index.ts
@@ -0,0 +1,10 @@
+import applications from './applications'
+import projects from './projects'
+import requisitions from './requisitions'
+const intermediate = {
+    applications: Object.assign(applications, applications),
+projects: Object.assign(projects, projects),
+requisitions: Object.assign(requisitions, requisitions),
+}
+
+export default intermediate
