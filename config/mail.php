58a393fffdeeaf177d68fbb76f1d5dbf1a79d04f
AWSYS-I\abijah.galwan <abijah.galwan@awsys-i.com>
2026-04-20 11:27:46 +0800

    added 'auto_tls' => false, to config/mail

--- config/mail.php
+++ config/mail.php
@@ -45,6 +45,7 @@ return [
             'port' => env('MAIL_PORT', 2525),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
+            'auto_tls' => false,
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
