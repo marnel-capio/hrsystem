664ba9ee6c43d463355958cdaf1a8123886d4388
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-04-23 09:34:53 +0800

    migration

--- database/migrations/2026_04_08_115411_rename_location_and_add_salary_column.php
+++ database/migrations/2026_04_08_115411_rename_location_and_add_salary_column.php
@@ -15,9 +15,12 @@ class RenameLocationAndAddSalaryColumn extends Migration
     {
         Schema::table('resource_requisitions', function (Blueprint $table) {
             $table->renameColumn('location_assignement', 'location_assignment');
-
             $table->renameColumn('preferred_skilss', 'preferred_skills');
             
+            // Add the new custom_location column
+            $table->string('custom_location', 255)->nullable()->after('role');  // Adjust the type/length as needed
+            
+            // Add the expected_salary_range column
             $table->string('expected_salary_range', 1024)->nullable()->after('role');
         });
     }
@@ -33,6 +36,10 @@ class RenameLocationAndAddSalaryColumn extends Migration
             $table->renameColumn('location_assignment', 'location_assignement');
             $table->renameColumn('preferred_skills', 'preferred_skilss');
             
+            // Drop the custom_location column
+            $table->dropColumn('custom_location');
+            
+            // Drop the expected_salary_range column
             $table->dropColumn('expected_salary_range');
         });
     }
