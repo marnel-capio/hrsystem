4ede19be4db2d653a1754368d4acb502d8d7efab
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-04-23 09:48:52 +0800

    rrf migration

--- database/migrations/2026_04_23_094642_add_custom_location_and_salary_range_to_resource_requisitions.php
+++ database/migrations/2026_04_23_094642_add_custom_location_and_salary_range_to_resource_requisitions.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RenameLocationAndAddSalaryColumn extends Migration
+class AddCustomLocationAndSalaryRangeToResourceRequisitions extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,8 @@ class RenameLocationAndAddSalaryColumn extends Migration
     public function up()
     {
         Schema::table('resource_requisitions', function (Blueprint $table) {
-            $table->renameColumn('location_assignement', 'location_assignment');
-            $table->renameColumn('preferred_skilss', 'preferred_skills');
-            
-            // Add the new custom_location column
-            $table->string('custom_location', 255)->nullable()->after('role');  // Adjust the type/length as needed
+            // Add the custom_location column
+            $table->string('custom_location', 255)->nullable()->after('role');
             
             // Add the expected_salary_range column
             $table->string('expected_salary_range', 1024)->nullable()->after('role');
@@ -33,9 +30,6 @@ class RenameLocationAndAddSalaryColumn extends Migration
     public function down()
     {
         Schema::table('resource_requisitions', function (Blueprint $table) {
-            $table->renameColumn('location_assignment', 'location_assignement');
-            $table->renameColumn('preferred_skills', 'preferred_skilss');
-            
             // Drop the custom_location column
             $table->dropColumn('custom_location');
             
