278bb4a3faa2c397bda649f18d7df31fe847832a
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-04-28 20:13:37 +0800

    rrf

--- resources/js/routes/intermediate/index.ts
+++ resources/js/routes/intermediate/index.ts
@@ -1,11 +1,11 @@
 import applications from './applications'
-import requisitions from './requisitions'
 import projects from './projects'
+import requisitions from './requisitions'
 import applicants from './applicants'
 const intermediate = {
     applications: Object.assign(applications, applications),
-requisitions: Object.assign(requisitions, requisitions),
 projects: Object.assign(projects, projects),
+requisitions: Object.assign(requisitions, requisitions),
 applicants: Object.assign(applicants, applicants),
 }
 
