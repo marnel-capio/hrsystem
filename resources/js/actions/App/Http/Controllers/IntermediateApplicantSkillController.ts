12cf0c8dd4ef8d89ad78e4c2b2fc9e25d57a9193
AWSYS-I\lauraine.pultz <lauraine.pultz@awsys-i.com>
2026-04-29 16:55:46 +0800

    final

--- resources/js/actions/App/Http/Controllers/IntermediateApplicantSkillController.ts
+++ resources/js/actions/App/Http/Controllers/IntermediateApplicantSkillController.ts
@@ -1,5 +1,4 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition, applyUrlDefaults } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\IntermediateApplicantSkillController::index
  * @see app/Http/Controllers/IntermediateApplicantSkillController.php:13
